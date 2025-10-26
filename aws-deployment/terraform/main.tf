eeef0dc87d5c6356df073aecb8b4e9704de65b61
bill-banfield <bill@ami-residual.com>
2025-10-26 17:37:21 -0500

    FIX: Update PostgreSQL version to supported AWS RDS version
    - Changed PostgreSQL version from 15.4 to 15.2
    - AWS RDS does not support PostgreSQL 15.4
    - Version 15.2 is a supported PostgreSQL version in AWS RDS
    - This should resolve the InvalidParameterCombination error

--- aws-deployment/terraform/main.tf
+++ aws-deployment/terraform/main.tf
@@ -183,7 +183,7 @@ resource "aws_db_instance" "main" {
   identifier = "${var.project_name}-postgres"
 
   engine         = "postgres"
-  engine_version = "15.4"
+  engine_version = "15.2"
   instance_class = var.db_instance_class
 
   allocated_storage     = 20
