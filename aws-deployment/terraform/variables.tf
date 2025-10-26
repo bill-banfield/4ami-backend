a01455bc3a531065cb731c9d2284d5991180496f
bill-banfield <bill@ami-residual.com>
2025-10-26 17:29:55 -0500

    FIX: RDS identifier must start with a letter
    - Changed project_name from '4ami-backend' to 'ami-backend'
    - RDS identifiers cannot start with numbers
    - This fixes the 'first character of identifier must be a letter' error
    - All AWS resources will now use 'ami-backend' prefix instead of '4ami-backend'

--- aws-deployment/terraform/variables.tf
+++ aws-deployment/terraform/variables.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "4ami-backend"
+  default     = "ami-backend"
 }
 
 # Database Variables
